020af45b33ea4a679867f98e3d03c832b20688b4
Ridho <111733815+fahmiridho07@users.noreply.github.com>
2025-11-18 14:44:00 +0700

    Readme

--- rec-service/run.ps1
+++ rec-service/run.ps1
@@ -1,11 +1,11 @@
 # run.ps1
 $ErrorActionPreference = "Stop"
 
-Write-Host "🚀 Starting FastAPI backend..."
+Write-Host "Starting FastAPI backend..."
 
 # aktifkan virtual environment
 if (-Not (Test-Path ".\venv\Scripts\Activate.ps1")) {
-    Write-Host "⚠️ venv not found, please run: python -m venv venv"
+    Write-Host "venv not found, please run: python -m venv venv"
     exit
 }
 .\venv\Scripts\Activate.ps1
